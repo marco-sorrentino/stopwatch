a91bad16101c9b5ee6ba2313f625d61c2e1e54a3
Marco-Sorrentino <sorrentmarco@gmail.com>
2022-12-31 17:02:09 +0100

    html done

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  let seconds = 00;
+  let tens = 00;
+  let appendTens = document.getElementById("tens");
+  let appendSeconds = document.getElementById("seconds");
+  let buttonStart = document.getElementById("start");
+  let buttonStop = document.getElementById("stop");
+  let buttonReset = document.getElementById("reset");
+};
